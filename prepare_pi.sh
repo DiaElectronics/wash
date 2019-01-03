a96a475bfa60debee1992294e868aa3b8536125f
rcsiberia <rcsrambler@gmail.com>
2019-01-02 21:21:52 -0800

    small fixes

--- prepare_pi.sh
+++ prepare_pi.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update -y && sudo apt-get upgrade -y
 
-sudo apt install -y redis=server libssl-dev libsdl-image1.2-dev libevent-dev libsdl1.2-dev
+sudo apt install -y redis-server libssl-dev libsdl-image1.2-dev libevent-dev libsdl1.2-dev
 
 sudo echo "@xset s off" >> /etc/xdg/lxsession/LXDE-pi/autostart
 sudo echo "@xset -dpms" >> /etc/xdg/lxsession/LXDE-pi/autostart
@@ -26,8 +26,7 @@ echo "./update.sh &" >> /home/pi/run.sh
 echo "sleep 1" >> /home/pi/run.sh
 echo "./firmware.exe_" >> /home/pi/run.sh
 
-cd /home/pi/SelfServiceWash/gpio_test
-./build.sh
+cd /home/pi/wash
 read -p "Enter Card Code : " card_code
 echo "$card_code" >> id.txt
 chmod 777 /home/pi/run.sh
