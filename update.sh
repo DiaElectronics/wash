19a16af68fdd1d7ad51f640a7dc2d15e24db1ffb
Roman Fedyashov <roman.fedyashov@gmail.com>
2019-08-10 14:53:26 +0700

    fixed sizes

--- update.sh
+++ update.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 echo "UPDATING..."
+
+LOGS="$(git pull|grep firmware.exe)"
+echo "LOGS: [""$LOGS""]"
+[ -z "$LOGS" ] || (echo "rebooting" && sleep 30 && sudo reboot)
+
+sleep 2
 LOGS="$(git pull|grep firmware.exe)"
 echo "LOGS: [""$LOGS""]"
 [ -z "$LOGS" ] || (echo "rebooting" && sleep 30 && sudo reboot)
